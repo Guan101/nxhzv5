9b3ab770ec7ee6df78e1aa414d201e6c5454f5e4
Guan101 <93143059+Guan101@users.noreply.github.com>
2023-06-03 21:23:08 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue"
+const msg = ref("Hello World")
+</script>
+
+<template>
+  <div>s
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# nxhzv5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nxhzv5)
